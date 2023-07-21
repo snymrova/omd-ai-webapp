a6575ba0eeaafec7d502c688ace6483a48ef1590
sunny luthra <luthra.sunny@gmail.com>
2023-07-21 12:38:04 +0530

    doc init

--- lessons/docusaurus.config.js
+++ lessons/docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://snymrova.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/lessons',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'snymrova', // Usually your GitHub org/user name.
-  projectName: 'omd-ai-webapp', // Usually your repo name.
+  projectName: 'snymrova.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
